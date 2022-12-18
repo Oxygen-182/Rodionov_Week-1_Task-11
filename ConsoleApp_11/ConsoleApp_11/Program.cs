dbccc754243d0699f76df038cf0259fb35c76da1
Oxygen-182 <119591922+Oxygen-182@users.noreply.github.com>
2022-12-18 13:35:19 +0300

    Add files via upload

---
+++ ConsoleApp_11/ConsoleApp_11/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_11
+{
+    internal class Program
+    {
+        class ThisString
+        {
+            private StringBuilder Line;
+            private int n;
+            public ThisString(string str)
+            {
+                Line = new StringBuilder(str);
+                n = str.Length;
+            }
+            public ThisString(string str, int len)
+            {
+                n = len;
+                Line = new StringBuilder(str.Substring(0, n));
+            }
+            public int CountSpaces()
+            {
+                return Line.ToString().Count(x => x == ' ');
+            }
+            public void RemovePuncts()
+            {
+                string str = Line.ToString();
+                Line.Clear();
+                Line.Append(Regex.Replace(str, "[,.:?!]", ""));
+                n = Line.Length;
+            }
+            public void Lower()
+            {
+                string str = Line.ToString();
+                Line.Clear();
+                Line.Append(Regex.Replace(str, @"[А-ЯЁ]", m => m.ToString().ToLower()));
+                n = Line.Length;
+            }
+            public int Length
+            {
+                get { return n; }
+            }
+            public StringBuilder line
+            {
+                get
+                {
+                    return Line;
+                }
+                set
+                {
+                    Line = value;
+                }
+            }
+            public override string ToString()
+            {
+                return Line.ToString();
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            string line;
+
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Введите строку: ");
+                    line = (Console.ReadLine());
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            ThisString s = new ThisString(line);
+            Console.WriteLine("\nСтрока: {0} \nДлина строки: {1}", s, s.Length);
+            Console.WriteLine("\nКоличество пробелов: {0}", s.CountSpaces());
+            s.Lower();
+            Console.WriteLine("\nЗамена прописных букв на строчные: {0}", s);
+            s.RemovePuncts();
+            Console.WriteLine("\nСтрока без знаков препинания: {0} \nДлина строки: {1}", s, s.Length);
+            Console.WriteLine("\nСтрока: {0}", s.line);
+        }
+    }
+}
---
+++ ConsoleApp_11_form/ConsoleApp_11_form/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsoleApp_11_form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        class ThisString
+        {
+            private StringBuilder Line;
+            private int n;
+            public ThisString(string str)
+            {
+                Line = new StringBuilder(str);
+                n = str.Length;
+            }
+            public int CountSpaces()
+            {
+                return Line.ToString().Count(x => x == ' ');
+            }
+            public void RemovePuncts()
+            {
+                string str = Line.ToString();
+                Line.Clear();
+                Line.Append(Regex.Replace(str, "[,.:?!]", ""));
+                n = Line.Length;
+            }
+            public void Prop()
+            {
+                string str = Line.ToString();
+                Line.Clear();
+                Line.Append(Regex.Replace(str, @"[А-ЯЁ]", m => " " + m.ToString().ToLower()));
+                n = Line.Length;
+            }
+            public int length
+            {
+                get { return n; }
+            }
+            public StringBuilder line
+            {
+                get
+                {
+                    return Line;
+                }
+                set
+                {
+                    Line = value;
+                }
+            }
+            public override string ToString()
+            {
+                return Line.ToString();
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox3.Clear();
+            string line;
+
+            while (true)
+            {
+                try
+                {
+                    line = textBox2.Text;
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    textBox3.Text += ex.Message;
+                }
+            }
+
+            ThisString s = new ThisString(line);
+            textBox3.Text += $"Строка: {s}" + Environment.NewLine + $"Длина строки: {s.length}" + Environment.NewLine;
+            textBox3.Text += Environment.NewLine + $"Количество пробелов: {s.CountSpaces()}" + Environment.NewLine;
+            s.Prop();
+            textBox3.Text += Environment.NewLine + $"Замена прописных букв на строчные: {s}" + Environment.NewLine;
+            s.RemovePuncts();
+            textBox3.Text += Environment.NewLine + $"Строка без знаков препинания: {s}" + Environment.NewLine + $"Длина строки: {s.length}" + Environment.NewLine;
+            textBox3.Text += Environment.NewLine + $"Строка: {s.line}" + Environment.NewLine;
+        }
+    }
+}
